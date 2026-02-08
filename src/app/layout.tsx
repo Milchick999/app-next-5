fe7eeb0d1bc2373915a47c9f753f808614b45fcf
Zinaida Rudak <rudakzinaida@gmail.com>
2026-02-08 15:22:15 +0300

    feat: Add application pages and navigation layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <nav className="flex gap-4 ">
+          <Link href="/">Home</Link>
+          <Link href="/posts">Posts</Link>
+          <Link href="/favourites">Favourites</Link>
+          <Link href="/login">Login</Link>
+        </nav>
+
+        <main>
+          {children}
+        </main>
+
       </body>
     </html>
   );
