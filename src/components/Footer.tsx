fe62892832d27032048374caf8674165a91e13b4
Zinaida Rudak <rudakzinaida@gmail.com>
2026-02-15 23:36:53 +0300

    ui: Implement global footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="border-t py-6">
+      <div className="max-w-3xl mx-auto px-4 text-center text-sm text-gray-500">
+        <p>MyBlog 2026</p>
+      </div>
+    </footer>
+  );
+}
